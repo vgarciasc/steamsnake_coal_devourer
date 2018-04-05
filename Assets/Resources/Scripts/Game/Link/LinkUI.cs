05996e2516c6ac1ae863288a90dae496832413a4
vgarciasc <vgarciasc@users.noreply.github.com>
2018-04-05 13:07:19 -0300

    v0.2.10
    added:
    - link has compass pointing to snake.

---
+++ Assets/Resources/Scripts/Game/Link/LinkUI.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class LinkUI : Photon.PunBehaviour {
+
+	LinkUICompass compass;
+	SteamsnakeHead steamsnakeHead;
+
+	void Start() {
+		bool isLink = (bool) photonView.owner.CustomProperties["is_link"];
+		bool isBoss = (bool) photonView.owner.CustomProperties["is_boss"];
+
+		compass = GameObject.FindGameObjectWithTag ("LinkCompass").GetComponentInChildren<LinkUICompass>();
+
+		if (isBoss) {
+			compass.gameObject.SetActive (false);
+			this.enabled = false;
+		}
+	}
+
+	void Update() {
+		if (steamsnakeHead == null) {
+			var obj = GameObject.FindGameObjectWithTag ("Snake");
+			if (obj != null) steamsnakeHead = obj.GetComponentInChildren<SteamsnakeHead> ();
+		} else {
+			HandleCompass();
+		}
+	}
+
+	void HandleCompass() {
+		Vector3 diff = (steamsnakeHead.transform.position - this.transform.position).normalized;
+		float angle = Vector2.Angle (Vector2.up, diff);
+		print (angle);
+		angle *= (diff.x < 0 ? 1 : -1);
+		print (angle);
+
+		compass.AngleTo( angle );
+	}
+}
---
+++ Assets/Resources/Scripts/Game/Link/LinkUICompass.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using DG.Tweening;
+
+public class LinkUICompass : MonoBehaviour {
+	public Image indicator;
+	public List<Image> circles;
+
+	void Start() {
+		StartCoroutine (_Start ());
+	}
+
+	IEnumerator _Start() {
+		yield return new WaitUntil(() => PhotonNetwork.player.CustomProperties["is_link"] != null);
+		if ((bool) PhotonNetwork.player.CustomProperties ["is_boss"]) {
+			this.gameObject.SetActive (false);
+			yield break;
+		}
+
+		StartCoroutine (StartCircles ());
+	}
+
+	IEnumerator StartCircles() {
+		for (int i = 0; i < circles.Count; i++) {
+			StartMovingCircle (circles[i]);
+			yield return new WaitForSeconds (1f);
+		}
+	}
+
+	void StartMovingCircle(Image circle) {
+		circle.color = new Color (1f, 1f, 1f, 0.5f);
+		circle.transform.localScale = Vector3.zero;
+		circle.transform.DOScale (Vector3.one, 2f);
+		circle.DOFade (0f, 2f).OnComplete(() => {
+			StartMovingCircle(circle);
+		});
+	}
+
+	public void AngleTo(float angle) {
+		indicator.transform.rotation = Quaternion.Euler (new Vector3 (0, 0, angle));
+	}
+}
